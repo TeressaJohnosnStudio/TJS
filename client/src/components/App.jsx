56f977118125e98a51002b26105c79da75b4f384
dpillay03 <dpillay03@gmail.com>
2018-06-11 21:18:27 -0700

    config files setup. html renduring to page

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'React';
+
+class App extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return <h1>Teressa Johnson Studio</h1>
+    }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 'use strict';
 const express = require('express');
 const app = express();
+const webpack = require('webpack');
+const compiler = webpack(require('./webpack.config.js'));
+const middleware = require('webpack-dev-middleware');
+const instance = middleware(compiler);
 
+app.use('client', express.static(`${__dirname}/client`));
+app.use(instance);
 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: `${__dirname}/`})
+})
 
+instance.waitUntilValid(() => {
+    console.log('package is valid');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
