2b138e9d04a4d045c37e6a721c2077c4d50866f6
dpillay03 <dpillay03@gmail.com>
2018-06-14 00:30:29 -0700

    input values are being logged into the console.

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,18 +1,60 @@
 import React from 'react';
 
 export default class Contact extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = {
+            name: '',
+            email: '',
+            subject: '',
+            question: ''
+        }
+        this.handleName = this.handleName.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handleSubject = this.handleSubject.bind(this);
+        this.handleQuestion = this.handleQuestion.bind(this);
+        this.handleAll = this.handleAll.bind(this);
     }
-    render(){
+
+    handleName(e) {
+        e.preventDefault();
+        let setName = e.target.value;
+        this.setState({ name: setName });
+        console.log('NAME: ', setName);
+        
+    }
+    handleEmail(e) {
+        e.preventDefault();
+        let setEmail = e.target.value;
+        this.setState({ email: setEmail });
+        console.log('EMAIL: ', setEmail);
+    }
+    handleSubject(e) {
+        e.preventDefault();
+        let setSubject = e.target.value;
+        this.setState({ subject: setSubject });
+        console.log('SUBJECT: ', setSubject);
+    }
+    handleQuestion(e) {
+        e.preventDefault();
+        let setQuestion = e.target.value;
+        this.setState({ question: setQuestion });
+        console.log('Question: ', setQuestion);
+    }
+
+    handleAll(e) {
+        e.preventDefault();
+    }
+
+    render() {
         return <React.Fragment>
             <div className="left">
                 <div className="img"></div>
                 <div className="media">
-                    <img className="icon"/>
-                    <img className="icon"/>
-                    <img className="icon"/>
-                    <img className="icon"/>
+                    <img className="icon" />
+                    <img className="icon" />
+                    <img className="icon" />
+                    <img className="icon" />
                 </div>
             </div>
 
@@ -20,13 +62,15 @@ export default class Contact extends React.Component {
                 <div className="title">
                     <p>contact</p>
                 </div>
-                <div className="description">It turns out that I enjoy meeting people and I love coffee. So if you like those same things, let's be friends. &#123;253&#125; 709–9509</div>
+                <div className="description">It turns out that I enjoy meeting people and I love coffee. So if you like those same things, let's be friends. &#123;253&#125; 709–9509
+                </div>
+
                 <div className="contact">
-                    <form>
-                        <input type="text" placeholder="name"></input>
-                        <input type="text" placeholder="email"></input>
-                        <input type="text" placeholder="subject"></input>
-                        <input type="text" placeholder="your question"></input>
+                    <form onSubmit={this.handleAll}>
+                        <input type="text" placeholder="name" onChange={this.handleName}></input>
+                        <input type="text" placeholder="email" onChange={this.handleEmail}></input>
+                        <input type="text" placeholder="subject" onChange={this.handleSubject}></input>
+                        <input type="text" placeholder="your question" onChange={this.handleQuestion}></input>
                         <button>Send Message</button>
                     </form>
                 </div>
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const webpack = require('webpack');
 const compiler = webpack(require('./webpack.config.js'));
 const middleware = require('webpack-dev-middleware');
+const nodemailer = require('nodemailer')
 const instance = middleware(compiler);
 
 app.use('client', express.static(`${__dirname}/client`));
@@ -16,6 +17,9 @@ app.get('/', (req, res) => {
 instance.waitUntilValid(() => {
     console.log('package is valid');
 });
+
+
+//CONTACT PAGE - EMAIL:
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
