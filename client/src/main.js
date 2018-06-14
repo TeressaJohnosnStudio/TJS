5686bde1e1ae7e9a820ffc3a8bc38370f30b0a9d
rjtj2007 <teacherman1@gmail.com>
2018-06-13 21:12:50 -0700

    changed liter to new configuration and adjusted all single quote marks with (&apos;)

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+// playing with the existing css file
+import '../../SandboxCSS/style.css';
 
-
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 ReactDOM.render(<App />, root);
