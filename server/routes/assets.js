43bc988a0f16cf93ea50d513e4b5be99687e6d48
rjtj2007 <teacherman1@gmail.com>
2018-06-16 10:56:43 -0700

    testing asset upload from router and model

--- server/routes/assets.js
+++ server/routes/assets.js
@@ -12,10 +12,32 @@ router.route('/assets')
       .then(assets => res.json(assets))
       .catch(err => res.send(err));
   })
-  .post((req, res) => {
-    Assets.create(req.body)
-      .then(assets => res.json(assets))
-      .catch(err => res.send(err));
+  .post(upload.single('assets'), (req, res, next) => {
+    console.log('GOT', req.file);
+    
+       next = path.extname(req.file.originalname);
+    
+       let params = {
+           // access control list
+           ACL: 'public-read',
+           Bucket: process.env.AWS_BUCKET,
+           Key: req.file.originalname,
+           Body: fs.createReadStream(req.file.path)
+       };
+       console.log('uploading...');
+       s3.upload(params, (err, s3Data) => {
+           console.log('uploaded', s3Data);
+           let asset = new Asset({
+               title: req.body.title,
+               content: req.body.content,
+               imageUrl: s3Data.Location
+           });
+           asset.save()
+           .then((asset) => {
+               console.log('saved', asset);
+               res.send(asset);
+           });
+       }); 
   });
 
 module.exports = router;
