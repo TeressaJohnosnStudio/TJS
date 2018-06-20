eec8e1a57a5e4845f392fa747ff0c5ec6caf1445
dpillay03 <dpillay03@gmail.com>
2018-06-19 20:43:18 -0700

    sending emails to all three flower selections. now I need work on hiding the elements

--- client/src/components/Subscription/index.js
+++ client/src/components/Subscription/index.js
@@ -77,21 +77,21 @@ export default class Subscription extends React.Component {
                     </form>
                 </div>
 
-                {/*<div id="option3">
+                <div id="option3">
                     <img />
                     <p>WHY, HELLO!</p>
                     <p>$300.00</p>
                     <p>Why, hello! Transform a space and respect its architecture, and for goodness sake, let's bring in some flowers! Dramatic, large, and boasting a large variety of blooms, this type of arrangement will turn heads and make them swoon. Perfect for grand entrances, reception desks, and spiraling staircases, this size will be a showstopper. All flowers and foliage are season specific, but of couse feel free to leave comments and we'll do our best to accommodate. American grown | Locally resourced | Delivered Thursdays</p>
-                    <form id="pickMe">
-                        <select className="flower-select">
-                            <option value="whyHello-weekly">Why, hello! : $300.00 USD - weekly </option>
-                            <option value="whyHello-monthly">Why, hello!  : $300.00 USD - monthly </option>
+                    <form id="whyHelo" onSubmit={this.handleSubmit}>
+                        <select name="flowerSelect" onChange={this.handleChange}>
+                            <option name="whyHelloWeekly" value="Why, Hello ---- WEEKLY">Why, hello! : $300.00 USD - weekly </option>
+                            <option name="whyHelloMonthly" value="Why, Hello ---- MONTHLY">Why, hello!  : $300.00 USD - monthly </option>
                         </select>
                         <p>Tell us more about your style and preferences (if you want to)</p>
-                        <input></input>
-                        <button>BRING IT HOME</button>
+                        <input name="question" type="text" onChange={this.handleChange}></input>
+                        <button type="submit">BRING IT HOME</button>
                     </form>
-                </div> */}
+                </div>
             </div>
         </React.Fragment>
     }
