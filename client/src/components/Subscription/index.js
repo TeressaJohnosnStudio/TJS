4f62f55adb886b59ba81015bdf3caeb43115fa75
dpillay03 <dpillay03@gmail.com>
2018-06-21 01:18:28 -0700

    pushing for project report pull request

--- client/src/components/Subscription/index.js
+++ client/src/components/Subscription/index.js
@@ -35,6 +35,10 @@ export default class Subscription extends React.Component {
         console.log('clicked')
         this.setState({ show: !this.state.show })
     }
+
+    falseOnLoad = () => {
+        this.setState({ show: this.state.show })
+    }
     render() {
         return <React.Fragment>
             <div className="wg">
@@ -50,11 +54,10 @@ export default class Subscription extends React.Component {
             </div>
 
             <div className="size-options">
-                <ul>
-                    <li>
-                        <div id="option1-container">
-                            <p onClick={this.toggleOptions}>IMAGE 1</p>
-                        </div>
+
+                <div>
+                    <div id="option1-container" onLoad={this.falseOnLoad} >
+                        <p onClick={this.toggleOptions}>IMAGE 1</p>
                         {this.state.show ?
                             <div id="option1"  >
                                 <p>YES, LOVELY.</p>
@@ -71,52 +74,57 @@ export default class Subscription extends React.Component {
                                 </form>
                             </div>
                             : null}
-                    </li>
-                </ul>
-
-                {/* <div id="option2">
-                    <ul>
-                        <li>
-                            <img />
-                            <p>OH, PICK ME!</p>
-                            <p>$150.00</p>
-                            <p>Ushering into thoughtful arrangement that which grows wild and free. These are the words to describe our aesthetic, and the words by which we design. Who wouldn't want to enter a space with such lovely thoughtfulness? Appealing to the eyes and to the imagination, this size is great for an entryway, foyer, or a living room and packed with sublime flowers that make an impressive statement. All flowers and foliage are season specific, but of course feel free to leave comments and we'll do our best to accommodate.
-                    American grown | Locally resourced | Delivered Thursdays</p>
-                            <form id="pickMe" onSubmit={this.handleSubmit}>
-                                <select name="flowerSelect" className="flowerSelect" onChange={this.handleChange}>
-                                    <option name="pickMeWeekly" value="Oh, pick me! ---- WEEKLY">Oh, pick me! : $150.00 USD - weekly </option>
-                                    <option name="pickMeMonthly" value="Oh, pick me! --- MONTHLY">Oh, pick me! : $150.00 USD - monthly </option>
-                                </select>
-                                <p>Tell us more about your style and preferences (if you want to)</p>
-                                <input name="question" type="text" onChange={this.handleChange}></input>
-                                <button type="submit">BRING IT HOME</button>
-                            </form>
-                        </li>
-                    </ul>
+                    </div>
                 </div>
 
-                <div id="option3">
-                    <ul>
-                        <li>
-                            <img />
-                            <p>WHY, HELLO!</p>
-                            <p>$300.00</p>
-                            <p>Why, hello! Transform a space and respect its architecture, and for goodness sake, let's bring in some flowers! Dramatic, large, and boasting a large variety of blooms, this type of arrangement will turn heads and make them swoon. Perfect for grand entrances, reception desks, and spiraling staircases, this size will be a showstopper. All flowers and foliage are season specific, but of couse feel free to leave comments and we'll do our best to accommodate. American grown | Locally resourced | Delivered Thursdays</p>
-                            <form id="whyHelo" onSubmit={this.handleSubmit}>
-                                <select name="flowerSelect" onChange={this.handleChange}>
-                                    <option name="whyHelloWeekly" value="Why, Hello ---- WEEKLY">Why, hello! : $300.00 USD - weekly </option>
-                                    <option name="whyHelloMonthly" value="Why, Hello ---- MONTHLY">Why, hello!  : $300.00 USD - monthly </option>
-                                </select>
-                                <p>Tell us more about your style and preferences (if you want to)</p>
-                                <input name="question" type="text" onChange={this.handleChange}></input>
-                                <button type="submit">BRING IT HOME</button>
-                            </form>
-                        </li>
-                    </ul>
-                </div> */}
 
+                <div>
+                    <div id="option2-container" onLoad={this.falseOnLoad}>
+                        <p onClick={this.toggleOptions}>IMAGE 2</p>
+                        {this.state.show ?
+                            <div id="option2">
+                                <p>OH, PICK ME!</p>
+                                <p>$150.00</p>
+                                <p>Ushering into thoughtful arrangement that which grows wild and free. These are the words to describe our aesthetic, and the words by which we design. Who wouldn't want to enter a space with such lovely thoughtfulness? Appealing to the eyes and to the imagination, this size is great for an entryway, foyer, or a living room and packed with sublime flowers that make an impressive statement. All flowers and foliage are season specific, but of course feel free to leave comments and we'll do our best to accommodate.
+                        American grown | Locally resourced | Delivered Thursdays</p>
+                                <form id="pickMe" onSubmit={this.handleSubmit}>
+                                    <select name="flowerSelect" className="flowerSelect" onChange={this.handleChange}>
+                                        <option name="pickMeWeekly" value="Oh, pick me! ---- WEEKLY">Oh, pick me! : $150.00 USD - weekly </option>
+                                        <option name="pickMeMonthly" value="Oh, pick me! --- MONTHLY">Oh, pick me! : $150.00 USD - monthly </option>
+                                    </select>
+                                    <p>Tell us more about your style and preferences (if you want to)</p>
+                                    <input name="question" type="text" onChange={this.handleChange}></input>
+                                    <button type="submit">BRING IT HOME</button>
+                                </form>
+                            </div>
+                            : null}
+                    </div>
+                </div>
+
+
+                <div id="option3">
+                    <div id="option3-container" onLoad={this.falseOnLoad}>
+                        <p onClick={this.toggleOptions}>IMAGE 2</p>
+                        {this.state.show ?
+                            <div id="option3">
+                                <img />
+                                <p>WHY, HELLO!</p>
+                                <p>$300.00</p>
+                                <p>Why, hello! Transform a space and respect its architecture, and for goodness sake, let's bring in some flowers! Dramatic, large, and boasting a large variety of blooms, this type of arrangement will turn heads and make them swoon. Perfect for grand entrances, reception desks, and spiraling staircases, this size will be a showstopper. All flowers and foliage are season specific, but of couse feel free to leave comments and we'll do our best to accommodate. American grown | Locally resourced | Delivered Thursdays</p>
+                                <form id="whyHelo" onSubmit={this.handleSubmit}>
+                                    <select name="flowerSelect" onChange={this.handleChange}>
+                                        <option name="whyHelloWeekly" value="Why, Hello ---- WEEKLY">Why, hello! : $300.00 USD - weekly </option>
+                                        <option name="whyHelloMonthly" value="Why, Hello ---- MONTHLY">Why, hello!  : $300.00 USD - monthly </option>
+                                    </select>
+                                    <p>Tell us more about your style and preferences (if you want to)</p>
+                                    <input name="question" type="text" onChange={this.handleChange}></input>
+                                    <button type="submit">BRING IT HOME</button>
+                                </form>
+                            </div>
+                            : null}
+                    </div>
+                </div>
             </div>
         </React.Fragment >
     }
-
 }
