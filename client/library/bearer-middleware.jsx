f936ade70088861a564836112bb807ae90e2c93f
rjtj2007 <teacherman1@gmail.com>
2018-06-22 11:04:42 -0700

    refactored bearerMiddleware file

--- client/library/bearer-middleware.jsx
+++ client/library/bearer-middleware.jsx
@@ -1,11 +1,16 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User.js');
+require('dotenv').config();
 
-function processBearer(req, res, next) {
+
+module.exports = (req, res, next) => {
   let authHeader = req.get('Authorization');
-  var token = authHeader.split('Bearer ')[1];
+  let token = authHeader.split('Bearer ')[1];
+
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
-    if (err) res.status(401).send('invalid token');
+    if (err) {
+      res.status(401).send('invalid token');
+    }
     User.findOne({_id: decoded.userId })
       .then(user => {
         req.user = user;
@@ -13,6 +18,4 @@ function processBearer(req, res, next) {
       })
       .catch(err => res.send(err.message));
   });
-}
-
-module.exports = processBearer;
+};
