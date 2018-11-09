c76583219a693e39810e88f090ba4258d615dac3
ryanscottjohnson <teacherman1@gmail.com>
2018-11-08 16:06:15 -0800

    refining navigation bar

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -12,15 +12,15 @@ export default class Navigation extends React.Component {
         <div id="navigation">
         <nav>
           <ul className="clearfix">
-            <li><Link to="/subscribe">Subscription</Link></li>
-            <li><Link to="/gallery">Gallery</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li id="info"><Link to="/info">Info</Link></li>
+            <li id="subscriptsions"><Link to="/subscribe">Subscriptions</Link></li>
+            <li id="gallery"><Link to="/gallery">Gallery</Link></li>
+            <li id="reviews"><Link to="/reviews">Press</Link></li>
+            {/* <li id="info"><Link to="/info">Info</Link></li> */}
             <li id="logo"><Link to="/"><img src={logo}/></Link></li>
-            <li><Link to="/faq">FAQ</Link></li>
-            <li><Link to="/reviews">Reviews</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            {/* <li><Link to="/blog">Blog</Link></li> */}
+            {/* <li><Link to="/faq">FAQ</Link></li> */}
+            <li id="about"><Link to="/about">About</Link></li>
+            <li id="contact"><Link to="/contact">Contact</Link></li>
+            <li id="blog"><Link to="/blog">Blog</Link></li>
           </ul>
           </nav>
         </div>
